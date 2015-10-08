192c6c79d0f28a4f0142a7a267513b4f59c1dfe7
eliasmedina <eliasmedina2010@hotmail.com>
2015-10-08 08:27:14 -0500

    preview
    Inicio del aplicativo logo de la universidad

---
+++ app/src/main/java/taxiwebserviquibdo/elienco/com/funlamweb/preview.java
@@ -0,0 +1,98 @@
+package taxiwebserviquibdo.elienco.com.funlamweb;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Environment;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+public class preview extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preview);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    String archivoinformacion=consultarArchivo("FunlamWeb");
+                    String informacion[] =  archivoinformacion.split("/");
+                    try{
+                        if(informacion[0].equals("OK")){
+                            Intent mainIntent = new Intent().setClass(preview.this, MainActivity.class);
+                            startActivity(mainIntent);
+                            finish();
+                        }
+                        else{
+                            Intent mainIntent = new Intent().setClass(preview.this,Login.class);
+                            startActivity(mainIntent);
+                            finish();
+                        }
+                    }
+                    catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            }
+        };
+        // Simulate a long loading process on application startup.
+        Timer timer = new Timer();
+        timer.schedule(task, 3000);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_preview, menu);
+        return true;
+    }
+    //Consulta la infomacion almacenada en los archivos
+    protected String consultarArchivo(String InfoArchivo)
+    {
+        File tarjeta = Environment.getExternalStorageDirectory();
+        File file = new File(tarjeta.getAbsolutePath(), InfoArchivo);
+        try {
+            FileInputStream fin = new FileInputStream(file);
+            InputStreamReader archivo = new InputStreamReader(fin);
+            BufferedReader br = new BufferedReader(archivo);
+            String linea = br.readLine();
+            String todo = "";
+            while (linea != null) {
+                todo = todo + linea + "";
+                linea = br.readLine();
+            }
+            br.close();
+            archivo.close();
+            return todo;
+        } catch (IOException e) {
+            return "Error";
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
