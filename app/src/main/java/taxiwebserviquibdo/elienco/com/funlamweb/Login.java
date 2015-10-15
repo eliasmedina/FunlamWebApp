28805d3aac3429e61937ce05a47c00eccad2dd73
jimmypelaez <jimmy.pelaez90@gmail.com>
2015-10-14 22:56:37 -0500

    Ajustes login
    Ajustes a la parte de login en campos de ingreso de informacion del
    usuario

--- app/src/main/java/taxiwebserviquibdo/elienco/com/funlamweb/Login.java
+++ app/src/main/java/taxiwebserviquibdo/elienco/com/funlamweb/Login.java
@@ -1,8 +1,12 @@
 package taxiwebserviquibdo.elienco.com.funlamweb;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,6 +14,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Login extends Activity {
     EditText usuario= null;
@@ -21,7 +33,6 @@ public class Login extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         Button login= (Button) findViewById(R.id.btnlogin);
-
         login.setOnClickListener(btnlogin_Click);
 
     }
@@ -36,7 +47,11 @@ public class Login extends Activity {
         }
         return super.onKeyDown(keyCode, event);
     }
-
+    public void btnregistro(View view){
+        Intent inten= new Intent(Login.this,registro.class);
+        startActivity(inten);
+        finish();
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -47,8 +62,8 @@ public class Login extends Activity {
     View.OnClickListener btnlogin_Click = new View.OnClickListener() {
         @Override
         public void onClick(View arg0) {
-            usuario = (EditText) findViewById(R.id.usuario);
-            login = (EditText) findViewById(R.id.con);
+             usuario = (EditText) findViewById(R.id.edtusuario);
+            login = (EditText) findViewById(R.id.contrasena);
             String a=usuario.getText().toString();
             String b=login.getText().toString();
             if (a.equals("")||b.equals("")) {
@@ -56,12 +71,43 @@ public class Login extends Activity {
             }else {
                 if(((usuario.getText().toString().matches("[a-zA-Z0-9._-]+@[a-z]+.[a-z]")&&usuario.length()> 0)||(usuario.getText().toString().matches("[a-zA-Z0-9._-]+@[a-z]+.[a-z]+.[a-z]")&&usuario.length()> 0)||(usuario.getText().toString().matches("[a-zA-Z0-9._-]+@[a-z]+.[a-z]+.co")&&usuario.length()> 0)||(usuario.getText().toString().matches("[a-zA-Z0-9._-]+@[a-z]+.co")&&usuario.length()> 0)||(usuario.getText().toString().matches("[a-zA-Z0-9._-]+@[a-z]+.es")&&usuario.length()> 0) ))
                     {
-                        if (usuario.toString() == "prueba@prueba.com" && login.toString() == "prueba") {
-                            Intent intlogin = new Intent(Login.this, MainActivity.class);
-                            startActivityForResult(intlogin, 0);
-                            finish();
+                        String archivoinformacion = consultarArchivo("FunlamWeb");
+                        String informacion[] = archivoinformacion.split("/");
+                        int reg=informacion.length;
+                        for (int i = 1; i < reg; i++) {
+                            String infor[] = informacion[i].split(",");
+                            int ger= infor.length;
+                            for(int j= 0; j<ger;j++){
+                                if(infor[1].equals(a)){
+                                    if(infor[2].equals((b))){
+                                        //guarda archivo en microsd
+                                        try {
+                                            String nomarchivo = "login";
+                                            File tarjeta = Environment.getExternalStorageDirectory();
+                                            File file = new File(tarjeta.getAbsolutePath(), nomarchivo);
+                                            OutputStreamWriter osw = new OutputStreamWriter(
+                                                    new FileOutputStream(file));
+                                            osw.write("OK,"+infor[0]+"/"+infor[1]+"/"+infor[2]+"/"+infor[3]+"/"+infor[4]+"/"+infor[5]+"/"+infor[6]);
+                                            osw.flush();
+                                            osw.close();
+                                            j=ger+1;
+                                            i=reg+1;
+                                            Toast.makeText(Login.this, "Usuario Correcto", Toast.LENGTH_SHORT).show();
+                                            Intent inten = new Intent(Login.this, MainActivity.class);
+                                            startActivity(inten);
+                                            finish();
+                                        }catch (Exception lol){
+                                            lol.printStackTrace();
+                                        }
+                                    }
+                                }else{
+                                    Toast.makeText(Login.this, "Usuario no existe o contraseña incorrecto", Toast.LENGTH_SHORT).show();
+                                }
 
-                }
+                            }
+
+                        }
+
                 }else {
                     Toast.makeText(Login.this, "valide su Correo", Toast.LENGTH_SHORT).show();
                 }
@@ -81,5 +127,28 @@ public class Login extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //Consulta la infomacion almacenada en los archivos
+    protected String consultarArchivo(String InfoArchivo)
+    {
+        File tarjeta = Environment.getExternalStorageDirectory();
+        File file = new File(tarjeta.getAbsolutePath(), InfoArchivo);
+        try {
+            FileInputStream fin = new FileInputStream(file);
+            InputStreamReader archivo = new InputStreamReader(fin);
+            BufferedReader br = new BufferedReader(archivo);
+            String linea = br.readLine();
+            String todo = "";
+            while (linea != null) {
+                todo = todo + linea + "";
+                linea = br.readLine();
+            }
+            br.close();
+            archivo.close();
+            return todo;
+        } catch (IOException e) {
+            return "Error";
+        }
     }
 }
