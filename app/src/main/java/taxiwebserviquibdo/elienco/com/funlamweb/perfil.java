c88731e73cbf50552c1a912bebb1febb18ba8409
williamg27 <williany24@hotmail.com>
2015-10-14 22:02:29 -0500

    Perfil
    Perfil terminada

--- app/src/main/java/taxiwebserviquibdo/elienco/com/funlamweb/perfil.java
+++ app/src/main/java/taxiwebserviquibdo/elienco/com/funlamweb/perfil.java
@@ -1,9 +1,26 @@
 package taxiwebserviquibdo.elienco.com.funlamweb;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class perfil extends Activity {
 
@@ -11,15 +28,63 @@ public class perfil extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
-    }
+        //guarda archivo en microsd
 
+
+        String archivoinformacion = consultarArchivo("login");
+        String informacion[] = archivoinformacion.split(",");
+        String infor[] = informacion[1].split("/");
+        TextView Correo = (TextView) findViewById(R.id.txtcorreo);
+        TextView Usuario = (TextView) findViewById(R.id.txtnombre);
+        TextView Celular = (TextView) findViewById(R.id.txtcelular);
+        TextView Cedula = (TextView) findViewById(R.id.txtcedula);
+        TextView Direccion = (TextView) findViewById(R.id.txtdireccion);
+       Correo.setText(infor[1]);
+        Usuario.setText(infor[3]);
+        Celular.setText(infor[4]);
+        Cedula.setText(infor[5]);
+        Direccion.setText(infor[6]);
+    }
+    //Consulta la infomacion almacenada en los archivos
+    protected String consultarArchivo(String InfoArchivo) {
+        File tarjeta = Environment.getExternalStorageDirectory();
+        File file = new File(tarjeta.getAbsolutePath(), InfoArchivo);
+        try {
+            FileInputStream fin = new FileInputStream(file);
+            InputStreamReader archivo = new InputStreamReader(fin);
+            BufferedReader br = new BufferedReader(archivo);
+            String linea = br.readLine();
+            String todo = "";
+            while (linea != null) {
+                todo = todo + linea + "";
+                linea = br.readLine();
+            }
+            br.close();
+            archivo.close();
+            return todo;
+        } catch (IOException e) {
+            return "Error";
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_perfil, menu);
         return true;
-    }
 
+
+    }
+    //boton atras
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK)) {
+            Intent nuevaAtivity = new Intent(perfil.this, MainActivity.class);
+            startActivity(nuevaAtivity);
+            finish();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
